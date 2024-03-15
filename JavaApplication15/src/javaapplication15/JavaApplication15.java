b06f7761c9874d2b5b5b80397a36145dbe35ab4f
aldenling <aldenling17@gmail.com>
2024-03-15 13:43:48 +0800

    walao

--- JavaApplication15/src/javaapplication15/JavaApplication15.java
+++ JavaApplication15/src/javaapplication15/JavaApplication15.java
@@ -16,6 +16,7 @@ public class JavaApplication15 {
     public static void main(String[] args) {
         System.out.print("hello world");
         System.out.print("hello AGAIN");
+        System.out.print("hello walao");
 
     }
 
