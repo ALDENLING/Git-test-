eb3ab8b271530ecf795792fd9076ebe35d1e702f
aldenling <aldenling17@gmail.com>
2024-03-15 13:41:22 +0800

    HELLO AGAIN

--- JavaApplication15/src/javaapplication15/JavaApplication15.java
+++ JavaApplication15/src/javaapplication15/JavaApplication15.java
@@ -15,6 +15,8 @@ public class JavaApplication15 {
      */
     public static void main(String[] args) {
         System.out.print("hello world");
+        System.out.print("hello AGAIN");
+
     }
-    
+
 }
